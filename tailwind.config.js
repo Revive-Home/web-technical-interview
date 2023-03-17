f544eb509d3430fc211eaf82cf974250ad91a591
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-03-16 20:30:37 -0700

    Completed first section of web technical challenge for revive real estate

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
       },
+      backgroundImage: {
+        'rows': "url('/public/house.jpg')"
+      }
     },
   },
   plugins: [],
