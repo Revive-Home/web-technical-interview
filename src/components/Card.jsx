bd57d3cf886944b681c8db069dbe6c753954f29d
Arllette Fonseca <arllette.fonseca@gmail.com>
2023-03-28 14:22:50 -0700

    added revive logo and working with alert styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,50 +10,54 @@ import HomesData from '../../data/homes.json'
 /* eslint-disable react/jsx-no-comment-textnodes */
 const Card = () => {
 
-
   return (
-    <div>
-    {HomesData.map(({id, address,bedrooms, bathrooms, city, state, zipCode, yearBuilt
+    <div className='flex flex-col mt-5 justify-center items-center md:flex-row '>
+    {HomesData.map(({id, address,bedrooms, bathrooms, city, state, zipCode, yearBuilt,sqft
     })=>{
-        return(
-           <div key={id} className=" w-[300px] p-6 relative mb-10 ">
-      <div>
-        <img
-          className=" rounded-xl w-full h-auto"
-          src="/images/house.jpeg"
-          alt="house"
-        ></img>
-        <div className="bg-slate-50 rounded-xl p-4 absolute bottom-[-15%] w-[84%]">
-          <div className="flex flex-col mb-2">
-            <div className="flex flex-row justify-between ">
-              <h1 className="font-black tracking-wider">{address}</h1>
-              <div className="bg-[#66c69c] flex items-center justify-center w-[20px]">
-                <BsChevronRight className="text-[white] " />
+        return (
+          <div
+            key={id}
+            className=" w-[300px] p-6 relative mb-10  hover:scale-105 ease in duration-300 "
+          >
+            <div>
+              <img
+                className=" rounded-xl w-full h-auto "
+                src="/images/house.jpeg"
+                alt="house"
+              ></img>
+              <div className="bg-slate-50 rounded-xl p-4 absolute bottom-[-15%] w-[84%]">
+                <div className="flex flex-col mb-2">
+                  <div className="flex flex-row justify-between ">
+                    <h1 className="font-black tracking-wider">{address}</h1>
+                    <div className="bg-[#66c69c] flex items-center justify-center w-[20px] cursor-pointer">
+                      <BsChevronRight className="text-[white] " />
+                    </div>
+                  </div>
+                  <p className="text-[gray]">
+                    {city}, {state} {zipCode}
+                  </p>
+                </div>
+                <div className="flex  flex-row justify-between">
+                  <div className="flex flex-row justify-evenly items-center   ">
+                    <FaBed />
+                    <p className="pl-1">{bedrooms}</p>
+                  </div>
+                  <div className="flex flex-row justify-evenly items-center   ">
+                    <BsFillCalendarEventFill />
+                    <p className="pl-1">{yearBuilt}</p>
+                  </div>
+                  <div className="flex flex-row justify-evenly items-center  ">
+                    <FaToilet />
+                    <p className="pl-1">{bathrooms}</p>
+                  </div>
+                  <div className="flex flex-row justify-evenly items-center ">
+                    <BsFillHouseDoorFill />
+                    <p className="pl-1">{sqft}</p>
+                  </div>
+                </div>
               </div>
             </div>
-            <p className="text-[gray]">{city}, {state} {zipCode}</p>
           </div>
-          <div className="flex  flex-row justify-between">
-            <div className="flex flex-row justify-evenly items-center w-[30px]  ">
-              <FaBed />
-              <p>{bedrooms}</p>
-            </div>
-            <div className="flex flex-row justify-evenly items-center  w-[50px] ">
-              <BsFillCalendarEventFill />
-              <p>{yearBuilt}</p>
-            </div>
-            <div className="flex flex-row justify-evenly items-center w-[30px] ">
-              <FaToilet />
-              <p>{bathrooms}</p>
-            </div>
-            <div className="flex flex-row justify-evenly items-center w-[30px] ">
-              <BsFillHouseDoorFill />
-              <p>2</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
         )
 
       })
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -8,6 +8,7 @@ const LoginPage = () => {
   const router = useRouter()
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const [success, setSuccess] = useState(false)
 
   const handleInputEmail = (e) => {
     const userEmail = e.target.value
@@ -32,41 +33,83 @@ const LoginPage = () => {
     }
   }
   return (
-    <div className="flex justify-evenly items-center h-screen">
-      <div>
-        <form className="flex flex-col justify-evenly ">
-          <label>Sign In</label>
-          <input
-            value={email}
-            onChange={handleInputEmail}
-            className="border my-2 px-1"
-            placeholder="Enter Email"
-          ></input>
-          <input
-            value={password}
-            onChange={handleInputPassword}
-            className="border my-2 px-1"
-            placeholder="Enter Password"
-          ></input>
-          <button
-            onClick={handleAunthentication}
-            className="my-2 border-2"
-            type="button"
-          >
-            Submit
-          </button>
-          <Link className="text-right text-[10px]" href="/">
-            Forgot Password
-          </Link>
-        </form>
+    <div className="flex justify-evenly items-center h-screen bg-[#f4f4f5] ">
+      <div className="border-2 border-[#373964] flex flex-col-reverse md:flex-row md:w-[1000px] h-screen w-full max-w-lg mx-auto justify-evenly items-center ">
+        <div>
+          <img
+            className="w-[100px] hidden md:block"
+            src="/images/revive.png"
+            alt="revive-logo"
+          />
+          <div className="b m-5">
+            <form className="flex flex-col justify-evenly ">
+              <label>Sign In</label>
+              <input
+                value={email}
+                onChange={handleInputEmail}
+                className="border my-2 px-1"
+                placeholder="Enter Email"
+              ></input>
+              <input
+                value={password}
+                onChange={handleInputPassword}
+                className="border my-2 px-1"
+                placeholder="Enter Password"
+              ></input>
+              <button
+                onClick={handleAunthentication}
+                className="my-2 border-2 bg-[#58BDA8] rounded-xl text-[white] p-2"
+                type="button"
+              >
+                Login
+              </button>
+              <Link href="/">
+                <p className="cursor-pointer text-right text-[10px]">
+                  Forgot Password
+                </p>
+              </Link>
+            </form>
+          </div>
+        </div>
+
+        <div>
+          <div className='flex justify-center md:hidden'>
+            <img
+            className="w-[150px] mb-5"
+            src="/images/revive.png"
+            alt="revive-logo"
+          />
+          </div>
+
+          <img
+            src="./images/house.jpeg"
+            alt="houseLogin"
+            className="w-[230px]"
+          ></img>
+        </div>
       </div>
-      <div>
-        <img
-          src="./images/house.jpeg"
-          alt="houseLogin"
-          className="w-[200px]"
-        ></img>
-      </div>
+      {success ? (
+        <div
+          class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded absolute top-0"
+          role="alert"
+        >
+          <strong class="font-bold">Success!</strong>
+          <span class="block sm:inline">Signed-on</span>
+          <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+            <svg
+              class="fill-current h-6 w-6 text-green-500"
+              role="button"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <title>Close</title>
+              <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+            </svg>
+          </span>
+        </div>
+      ) : (
+        <p></p>
+      )}
     </div>
   )
 }
