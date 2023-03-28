641f5a49bb285fa489b4cbd332d48c083cc8c521
Arllette Fonseca <arllette.fonseca@gmail.com>
2023-03-28 14:51:11 -0700

    added show success and error alert state and functionality

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
 import { useRouter } from "next/router"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import UsersData from "../../data/users.json"
 /* eslint-disable @next/next/no-img-element */
 
@@ -8,7 +8,17 @@ const LoginPage = () => {
   const router = useRouter()
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
-  const [success, setSuccess] = useState(false)
+  const [showAlertSuccess, setShowAlertSuccess] = useState(false)
+  const [showAlertError, setShowAlertError] = useState(false)
+
+  useEffect(()=>{
+    if(showAlertSuccess) {
+      const timeout = setTimeout(()=>{
+        setShowAlertSuccess(false)
+      },3500)
+      return()=>clearTimeout(timeout)
+    }
+  }, [showAlertSuccess])
 
   const handleInputEmail = (e) => {
     const userEmail = e.target.value
@@ -22,6 +32,7 @@ const LoginPage = () => {
     let userFound = false;
     UsersData.map((user) => {
       if (user.email === email.toLowerCase() && user.password === password) {
+        setShowAlertSuccess(true)
         userFound = true
         router.push("/UserPage")
         return
@@ -29,7 +40,7 @@ const LoginPage = () => {
     })
 
     if (!userFound) {
-      alert("incorrect")
+      setShowAlertError(true)
     }
   }
   return (
@@ -73,12 +84,12 @@ const LoginPage = () => {
         </div>
 
         <div>
-          <div className='flex justify-center md:hidden'>
+          <div className="flex justify-center md:hidden">
             <img
-            className="w-[150px] mb-5"
-            src="/images/revive.png"
-            alt="revive-logo"
-          />
+              className="w-[150px] mb-5"
+              src="/images/revive.png"
+              alt="revive-logo"
+            />
           </div>
 
           <img
@@ -88,24 +99,28 @@ const LoginPage = () => {
           ></img>
         </div>
       </div>
-      {success ? (
-        <div
-          class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded absolute top-0"
+      {showAlertSuccess ? (
+        <div className='absolute top-2'>
+          <div
+          className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative"
           role="alert"
         >
-          <strong class="font-bold">Success!</strong>
-          <span class="block sm:inline">Signed-on</span>
-          <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-            <svg
-              class="fill-current h-6 w-6 text-green-500"
-              role="button"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-            >
-              <title>Close</title>
-              <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-            </svg>
-          </span>
+          <strong className="font-bold">Success! </strong>
+          <span>Signed-on</span>
+        </div>
+        </div>
+      ) : (
+        <p></p>
+      )}
+      {showAlertError ? (
+        <div className='absolute top-2'>
+          <div
+          className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+          role="alert"
+        >
+          <strong className="font-black">Error! </strong>
+          <span>Check email or password</span>
+        </div>
         </div>
       ) : (
         <p></p>
