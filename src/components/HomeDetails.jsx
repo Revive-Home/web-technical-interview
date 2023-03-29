8ea45cf1148411c6fa02568008048a28ad9abcc3
Arllette Fonseca <arllette.fonseca@gmail.com>
2023-03-28 23:14:50 -0700

    finished up home details page demo

--- src/components/HomeDetails.jsx
+++ src/components/HomeDetails.jsx
@@ -3,10 +3,26 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { FaToilet } from 'react-icons/fa'
 import { MdBed,MdSquareFoot } from 'react-icons/md'
+import { useEffect } from 'react'
 
 const HomeDetails = () => {
   const router = useRouter()
-  const {address, imageUrl,bathrooms,bedrooms} = router.query
+  //getting params from url
+  const {address, imageUrl,bathrooms,bedrooms,sqft} = router.query
+
+  useEffect(() => {
+    //clearing local storage if user refreshes page
+    window.onbeforeunload = () => {
+      localStorage.clear()
+    }
+    //setting user storage as variable user
+    let user = JSON.parse(localStorage.getItem("user"))
+    //if the user exists set the firstname state if not take user back to login page
+    if (!user) {
+    router.push("/LoginPage")
+    }
+  }, [router])
+
   return (
     <div className="w-full">
       <div className="w-screen h-[30vh] md:h-[40vh] relative overflow-hidden ">
@@ -28,9 +44,8 @@ const HomeDetails = () => {
         <div className="col-span-4 sm:col-span-1 md:col-span-4 lg:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4 ">
           <p className='flex flex-row items-center'><MdBed className="m-2" /> {bedrooms} Bedrooms</p>
           <p className='flex flex-row items-center'><FaToilet className="m-2" /> {bathrooms} Baths</p>
-          <p className='flex flex-row items-center'><MdBed className="m-2" /> {bedrooms} SquareFeet</p>
-          <FaToilet className="m-2" />
-          <MdSquareFoot className="m-2" />
+          <p className='flex flex-row items-center'><MdSquareFoot className="m-2" /> {sqft} sq.ft</p>
+
         </div>
       </div>
       <Link href="/UserPage" className="ml-5">
